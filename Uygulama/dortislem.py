3aa6ea2d0d6e4d6a8a72290f2d8859a29be7c991
Nermin ÖZCAN <42169824+Nrmnzcn@users.noreply.github.com>
2020-10-06 16:11:57 +0300

    Add files via upload

---
+++ Uygulama/dortislem.py
@@ -0,0 +1,20 @@
+def toplama(x,y):
+    
+    toplam = x+y
+    return toplam
+
+
+def cikarma(x,y):
+
+    fark = x-y
+    return fark
+
+def carpma(x,y):
+
+    carpım = x*y
+    return carpım
+
+def bolme(x,y):
+
+    bolum = x/y
+    return bolum
